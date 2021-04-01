9692520f8735df586ebf0186792d3fecd90098d2
sohagmahin <sohagmahin@gmail.com>
2021-04-01 15:34:37 +0600

    L14 v5 Dispatching actions

--- redux-basic.js
+++ redux-basic.js
@@ -9,13 +9,30 @@ const initState = {
 
 // reducer
 const rootReducer = (state = initState, action) => {
+    if (action.type == "INT_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type == "ADD_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 }
 
 // store
 const store = createStore(rootReducer);
-
 console.log(store.getState());
 
 
 // dispatch action
+
+store.dispatch({ type: "INT_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
+
+// subscriptions
