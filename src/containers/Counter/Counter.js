c617b0f8bb34fd83c360c710260b146b13d8c617
sohagmahin <sohagmahin@gmail.com>
2021-04-09 17:19:21 +0600

    L14 v10 Dispathing Actions from within the components

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -30,10 +30,10 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler('inc')} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
             </div>
         );
     }
@@ -43,7 +43,16 @@ class Counter extends Component {
 const mapStateToProps = state => {
     return {
         ctr: state.counter
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+        onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+        onAddCounter: () => dispatch({ type: "ADD" }),
+        onSubtractCounter: () => dispatch({ type: "SUB" })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,27 @@ const initState = {
 };
 
 const reducer = (state = initState, action) => {
+    if (action.type === "INCREMENT") {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === "DECREMENT") {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === "ADD") {
+        return {
+            counter: state.counter + 5
+        }
+    }
+    if (action.type === "SUB") {
+        return {
+            counter: state.counter - 5
+        }
+    }
     return state;
 };
 
